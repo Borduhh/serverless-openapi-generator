aceee624cd684b63f9261604fc6a37c16b71f711
Nick Bordeau <nickbordeau@gmail.com>
2021-07-18 01:14:29 -0400

    ci: fixed release config

--- release.config.js
+++ release.config.js
@@ -3,8 +3,8 @@ module.exports = {
     branches: ['main'],
   },
   plugins: [
+    '@semantic-release/changelog',
     [
-      '@semantic-release/changelog',
       '@semantic-release/git',
       {
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
